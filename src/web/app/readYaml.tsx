7821e2b34f3237362fbd9154cac453fe8b611f29
songdabin <thdekqls1222@naver.com>
2024-12-20 17:07:52 +0900

    feat: camera model로 데이터 저장
    - 선언한 camera model을 이용한 데이터 저장

--- src/web/app/readYaml.tsx
+++ src/web/app/readYaml.tsx
@@ -1,9 +1,23 @@
 import { loadYamlFileSync } from "load-yaml-file";
+import { Camera } from "./cameraModel";
 
 async function readYaml(fileName: string) {
   const lData: any = await loadYamlFileSync(fileName);
 
-  return lData;
+  var rtnVal: Camera = new Camera(
+    lData.channel,
+    lData.sensor,
+    lData.distortion_model,
+    lData.hfov,
+    lData.height,
+    lData.width,
+    lData.intrinsic,
+    lData.vcs_extrinsic,
+    lData.lcs_extrinsic,
+    lData.mvcs_extrinsic
+  );
+
+  return rtnVal;
 }
 
 export { readYaml };
