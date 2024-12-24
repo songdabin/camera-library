d33efa310f94b4dea2be8a6da7603ddbf03b950d
songdabin <thdekqls1222@naver.com>
2024-12-24 18:49:22 +0900

    feat: readYaml 사용 테스트
    - readYaml 사용 테스트

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,10 +1,12 @@
 import { readYaml } from "camera-library/dist/read_yaml.js";
-
-let data = readYaml("../../lib/assets/svc_front.yaml");
-
-console.log(data);
+import path from "path";
 
 export default function Home() {
+  const filePath = path.join(process.cwd(), "assets", "svc_front.yaml");
+  const data = readYaml(filePath);
+
+  console.log(data);
+
   return (
     <html>
       <body>
