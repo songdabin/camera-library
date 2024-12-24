429697084df7e9a0d81ccc6104a0ecaf16ad3904
songdabin <thdekqls1222@naver.com>
2024-12-24 11:09:51 +0900

    fix: Next.js project setting
    - set a Next.js project to test camera-library

---
+++ app/app/page.tsx
@@ -0,0 +1,15 @@
+import { readYaml } from "camera-library";
+
+let data = readYaml("./assets/svc_front.yaml");
+
+console.log(data);
+
+export default function Home() {
+  return (
+    <html>
+      <body>
+        <div>Home</div>
+      </body>
+    </html>
+  );
+}
