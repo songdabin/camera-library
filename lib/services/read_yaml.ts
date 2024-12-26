352939c884167be49f9289d2906eccab7a6db3b2
songdabin <thdekqls1222@naver.com>
2024-12-26 11:02:27 +0900

    feat: split the yaml data
    - split the yaml data by ":".

--- lib/services/read_yaml.ts
+++ lib/services/read_yaml.ts
@@ -2,7 +2,9 @@ import * as fs from "fs";
 
 export function readYaml(fileName: string) {
   let fileContent = fs.readFileSync(fileName, "utf8");
-  console.log("file content/n", fileContent);
+
+  let splitFileContent = fileContent.split(":");
+  console.log(splitFileContent[0]);
 
   return fileContent;
 }
