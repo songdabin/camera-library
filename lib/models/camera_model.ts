913177fe42ba0014dc7764999c59bd3d2e81c459
songdabin <thdekqls1222@naver.com>
2024-12-26 10:57:07 +0900

    feat: camera model 생성
    - camera schema 및 model 생성

---
+++ lib/models/camera_model.ts
@@ -0,0 +1,68 @@
+interface Extrinsic {
+  frameFrom: string;
+  frameTo: string;
+  qw: number;
+  qx: number;
+  qy: number;
+  qz: number;
+  tx: number;
+  ty: number;
+  tz: number;
+}
+
+interface Intrinsic {
+  fx: number;
+  fy: number;
+  cx: number;
+  cy: number;
+  k1: number;
+  k2: number;
+  k3: number;
+  k4: number;
+  k5?: number;
+  k6?: number;
+  p1: number;
+  p2: number;
+}
+
+export class CameraModel {
+  channel: string;
+  sensor: string;
+  distortionModel: string;
+  hfov: number;
+  height: number;
+  width: number;
+  intrinsic: Intrinsic;
+  vcsExtrinsic: Extrinsic;
+  lcsExtrinsic: Extrinsic;
+  mvcsExtrinsic: Extrinsic;
+
+  constructor(
+    channel: string,
+    sensor: string,
+    distortionModel: string,
+    hfov: number,
+    height: number,
+    width: number,
+    intrinsic: Intrinsic,
+    vcsExtrinsic: Extrinsic,
+    lcsExtrinsic: Extrinsic,
+    mvcsExtrinsic: Extrinsic
+  ) {
+    const updatedIntrinsic = {
+      ...intrinsic,
+      k5: intrinsic.k5 ?? 0,
+      k6: intrinsic.k6 ?? 0,
+    };
+    this.channel = channel;
+    this.sensor = sensor;
+    this.distortionModel = distortionModel;
+    this.hfov = hfov;
+    this.height = height;
+    this.width = width;
+    this.intrinsic = updatedIntrinsic;
+    this.vcsExtrinsic = vcsExtrinsic;
+    this.lcsExtrinsic = lcsExtrinsic;
+    this.mvcsExtrinsic = mvcsExtrinsic;
+  }
+}
