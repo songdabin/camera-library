9b91620f31977574e3df7cd620bafa9b8488e69a
songdabin <thdekqls1222@naver.com>
2024-12-26 11:01:28 +0900

    fix: update import location
    - update import location due to the camera-library version change

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,9 @@
-import { readYaml } from "camera-library/dist/read_yaml.js";
+import { readYaml } from "camera-library/dist/services/read_yaml.js";
 import path from "path";
 
 export default function Home() {
   const filePath = path.join(process.cwd(), "assets", "svc_front.yaml");
   const data = readYaml(filePath);
-
-  console.log(data);
 
   return (
     <html>
