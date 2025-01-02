970bf3a98627369a7c1379211f879004bd84cf18
songdabin <thdekqls1222@naver.com>
2025-01-02 16:45:43 +0900

    chore: delete duplicated code
    - delete duplicated code

--- lib/services/read_yaml.ts
+++ lib/services/read_yaml.ts
@@ -1,24 +1,8 @@
 import * as fs from "fs";
 import { parseYaml } from "./parse_yaml";
-import { CameraModel } from "../models/camera_model";
 
 export function readYaml(fileName: string) {
   const fileContent = fs.readFileSync(fileName, "utf8");
 
-  const parsedYaml = parseYaml(fileContent);
-
-  const camera = new CameraModel(
-    parsedYaml.channel,
-    parsedYaml.sensor,
-    parsedYaml.distortionModel,
-    parsedYaml.hfov,
-    parsedYaml.height,
-    parsedYaml.width,
-    parsedYaml.intrinsic,
-    parsedYaml.vcsExtrinsic,
-    parsedYaml.lcsExtrinsic,
-    parsedYaml.mvcsExtrinsic
-  );
-
-  return camera;
+  return parseYaml(fileContent);
 }
