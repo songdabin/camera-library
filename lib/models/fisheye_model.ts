81db174c09ce37aa3ad45108166609d282980155
songdabin <thdekqls1222@naver.com>
2025-01-06 17:52:29 +0900

    feat: export class
    - export class

--- lib/models/fisheye_model.ts
+++ lib/models/fisheye_model.ts
@@ -1,6 +1,6 @@
 import { CameraModel, IParam, IReturn } from "./camera_model";
 
-class FisheyeModel extends CameraModel {
+export class FisheyeModel extends CameraModel {
   public projectCCSToICS(vec3: IParam): IReturn {
     return { x: 3, y: 3, isInImage: true };
   }
--- lib/models/rectilinear_model.ts
+++ lib/models/rectilinear_model.ts
@@ -1,6 +1,6 @@
 import { CameraModel, IParam, IReturn } from "./camera_model";
 
-class RectilinearModel extends CameraModel {
+export class RectilinearModel extends CameraModel {
   public projectCCSToICS(vec3: IParam): IReturn {
     return { x: 3, y: 3, isInImage: true };
   }
