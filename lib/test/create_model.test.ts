17e5b871909179c102138ceb2b1c87a1d20a7868
songdabin <thdekqls1222@naver.com>
2025-01-07 16:28:15 +0900

    feat: update test code
    - update test code

--- lib/test/create_model.test.ts
+++ lib/test/create_model.test.ts
@@ -1,11 +1,23 @@
+import path = require("path");
 import { FisheyeModel } from "../models/fisheye_model";
 import { RectilinearModel } from "../models/rectilinear_model";
-import { CreateModel } from "../services/create_model";
+import { createModel } from "../services/create_model";
+import { readFile } from "../services/read_file";
+import { splitData } from "../services/split_data";
 
 test("Fisheye Model Create Test", () => {
-  expect(CreateModel("svc_front.yaml")).toBeInstanceOf(FisheyeModel);
+  const filePath = path.join(process.cwd(), "assets", "svc_front.yaml");
+  const fileContent = readFile(filePath);
+  const [cameraType, cameraParams] = splitData(fileContent);
+
+  expect(createModel(cameraType, cameraParams)).toBeInstanceOf(FisheyeModel);
 });
 
 test("Rectilinear Model Create Test", () => {
-  expect(CreateModel("svc_rear.yaml")).toBeInstanceOf(RectilinearModel);
+  const filePath = path.join(process.cwd(), "assets", "svc_rear.yaml");
+  const fileContent = readFile(filePath);
+  const [cameraType, cameraParams] = splitData(fileContent);
+  expect(createModel(cameraType, cameraParams)).toBeInstanceOf(
+    RectilinearModel
+  );
 });
