4f25bdc44b2e99a71b8ad621bdfe67704c1d3697
songdabin <thdekqls1222@naver.com>
2025-01-07 16:26:20 +0900

    feat: update createModel
    - update createModel to use factory method design pattern

--- lib/services/create_model.ts
+++ lib/services/create_model.ts
@@ -1,39 +1,38 @@
-import * as fs from "fs";
-import path = require("path");
 import { FisheyeModel } from "../models/fisheye_model";
 import { RectilinearModel } from "../models/rectilinear_model";
-import { splitData } from "./split_data";
+import { CameraModel } from "../models/camera_model";
+import { CameraType } from "../types/type";
 
-export function CreateModel(fileName: string) {
-  const filePath = path.join(process.cwd(), "assets", `${fileName}`);
-  const fileContent = fs.readFileSync(filePath, "utf8");
-  const data = splitData(fileContent);
-
-  if (data[0].startsWith("standard")) {
+export function createModel(
+  cameraTypeName: CameraType,
+  cameraParams: CameraModel
+) {
+  if (cameraTypeName === "standard") {
     return new RectilinearModel(
-      data[1].channel,
-      data[1].sensor,
-      data[1].distortionModel,
-      data[1].hfov,
-      data[1].height,
-      data[1].width,
-      data[1].intrinsic,
-      data[1].vcsExtrinsic,
-      data[1].lcsExtrinsic,
-      data[1].mvcsExtrinsic
+      cameraParams.channel,
+      cameraParams.sensor,
+      cameraParams.distortionModel,
+      cameraParams.hfov,
+      cameraParams.height,
+      cameraParams.width,
+      cameraParams.intrinsic,
+      cameraParams.vcsExtrinsic,
+      cameraParams.lcsExtrinsic,
+      cameraParams.mvcsExtrinsic
     );
-  } else if (data[0].startsWith("fisheye")) {
+  }
+  if (cameraTypeName === "fisheye") {
     return new FisheyeModel(
-      data[1].channel,
-      data[1].sensor,
-      data[1].distortionModel,
-      data[1].hfov,
-      data[1].height,
-      data[1].width,
-      data[1].intrinsic,
-      data[1].vcsExtrinsic,
-      data[1].lcsExtrinsic,
-      data[1].mvcsExtrinsic
+      cameraParams.channel,
+      cameraParams.sensor,
+      cameraParams.distortionModel,
+      cameraParams.hfov,
+      cameraParams.height,
+      cameraParams.width,
+      cameraParams.intrinsic,
+      cameraParams.vcsExtrinsic,
+      cameraParams.lcsExtrinsic,
+      cameraParams.mvcsExtrinsic
     );
   }
 }
