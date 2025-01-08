b4739f72322923506b5dd3677b6dfdc2fda296ec
songdabin <thdekqls1222@naver.com>
2025-01-08 15:34:23 +0900

    chore: delete duplicated parse code
    - delete duplicated parse code

--- lib/services/parse_yaml.ts
+++ lib/services/parse_yaml.ts
@@ -1,51 +1,20 @@
+import { z } from "zod";
 import { cameraSchema } from "../types/camera_schema";
-import { ValidationError } from "../models/validation_error";
-import { channelOrFrameToEnum, frameFromEnum } from "../types/schema";
 import { FisheyeModel } from "../models/fisheye_model";
 import { RectilinearModel } from "../models/rectilinear_model";
-import { validateYaml } from "./validate_yaml";
+import { ValidationError } from "../models/validation_error";
 
 export function parseYaml(cameraParams: FisheyeModel | RectilinearModel) {
-  const channelValidationError = validateYaml(
-    channelOrFrameToEnum,
-    cameraParams.channel,
-    "channel"
-  );
-  if (channelValidationError) {
-    throw new ValidationError(channelValidationError);
-  }
+  try {
+    cameraSchema.parse(cameraParams);
+  } catch (validateError) {
+    if (validateError instanceof z.ZodError) {
+      const errorMessage = validateError.errors.map(
+        (e) => `${e.code}/ ${e.path}/ ${e.message}`
+      );
 
-  const extrinsicList = [
-    cameraParams.vcsExtrinsic,
-    cameraParams.lcsExtrinsic,
-    cameraParams.mvcsExtrinsic,
-  ];
-
-  for (const extrinsic of extrinsicList) {
-    const frameToValidationError = validateYaml(
-      channelOrFrameToEnum,
-      extrinsic.frameTo,
-      "frame_to"
-    );
-    if (frameToValidationError) {
-      throw new ValidationError(frameToValidationError);
+      throw new ValidationError("[Validation Error] " + errorMessage);
     }
-  }
-
-  for (const extrinsic of extrinsicList) {
-    const frameFromValidationError = validateYaml(
-      frameFromEnum,
-      extrinsic.frameFrom,
-      "frame_from"
-    );
-    if (frameFromValidationError) {
-      throw new ValidationError(frameFromValidationError);
-    }
-  }
-
-  const cameraSchemaValidationError = validateYaml(cameraSchema, cameraParams);
-  if (cameraSchemaValidationError) {
-    throw new ValidationError(cameraSchemaValidationError);
   }
 
   return cameraParams;
