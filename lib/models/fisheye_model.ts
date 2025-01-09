e96b033b9775cdf376ac09f852d5763c7fec7d9a
songdabin <thdekqls1222@naver.com>
2025-01-09 14:51:26 +0900

    feat: Fisheye projectCcsToIcs
    - fisheye projectCcsToIcs code

--- lib/models/fisheye_model.ts
+++ lib/models/fisheye_model.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { ICSPoint, Intrinsic, Vector3Like } from "../types/type";
+import { ICSPoint } from "../types/type";
 import { CameraModel } from "./camera_model";
 
 export type CcsToIcsPointsArgs = {
@@ -8,17 +8,15 @@ export type CcsToIcsPointsArgs = {
 
 export class FisheyeModel extends CameraModel {
   public distortThetas(theta: number) {
+    const { k1, k2, k3, k4 } = this.intrinsic;
+
     const theta2 = theta ** 2;
     const theta4 = theta ** 4;
     const theta6 = theta2 * theta4;
     const theta8 = theta4 * theta4;
+
     const thetaD =
-      theta *
-      (1 +
-        this.intrinsic.k1 * theta2 +
-        this.intrinsic.k2 * theta4 +
-        this.intrinsic.k3 * theta6 +
-        this.intrinsic.k4 * theta8);
+      theta * (1 + k1 * theta2 + k2 * theta4 + k3 * theta6 + k4 * theta8);
 
     return thetaD;
   }
@@ -28,13 +26,11 @@ export class FisheyeModel extends CameraModel {
     const theta = Math.acos(uswPoint.z);
     const phi = Math.atan2(uswPoint.y, uswPoint.x);
 
+    const { fx, fy, cx, cy, k1, k2, k3, k4, k5, k6 } = this.intrinsic;
     const fov = this.hfov;
+
     const thetaSlope =
-      1 +
-      this.intrinsic.k1 * fov ** 2 +
-      this.intrinsic.k2 * fov ** 4 +
-      this.intrinsic.k3 * fov ** 6 +
-      this.intrinsic.k4 * fov ** 8;
+      1 + k1 * fov ** 2 + k2 * fov ** 4 + k3 * fov ** 6 + k4 * fov ** 8;
 
     const distScale =
       theta < fov ? this.distortThetas(theta) : theta * thetaSlope;
@@ -42,9 +38,10 @@ export class FisheyeModel extends CameraModel {
     const dnx = distScale * Math.cos(phi);
     const dny = distScale * Math.sin(phi);
 
-    const x = this.intrinsic.fx * dnx + this.intrinsic.cx;
-    const y = this.intrinsic.fy * dny + this.intrinsic.cy;
+    const x = fx * dnx + cx;
+    const y = fy * dny + cy;
     const isInImage = x >= 0 && x < this.width && y >= 0 && y < this.height;
+
     return { x, y, isInImage };
   }
 }
