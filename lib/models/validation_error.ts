8c1149de5ac4227077b895517fc582f9339b1e2f
songdabin <thdekqls1222@naver.com>
2025-01-13 14:43:45 +0900

    chore: delete custom error
    - use default zodError rather than validation_error(custom error)
    - custom error is almost same with zodError, so there's no need to use custom error in this situation.

--- lib/models/validation_error.ts
+++
@@ -1,6 +0,0 @@
-export class ValidationError extends Error {
-  constructor(message: string | undefined) {
-    super(message);
-    this.name = "ValidationError";
-  }
-}
--- lib/services/parse_yaml.ts
+++ lib/services/parse_yaml.ts
@@ -1,20 +1,8 @@
-import { z } from "zod";
 import { cameraSchema } from "../types/camera_schema";
-import { ValidationError } from "../models/validation_error";
 import { CameraModelType } from "../types/type";
 
 export function parseYaml(cameraParams: CameraModelType) {
-  try {
-    cameraSchema.parse(cameraParams);
-  } catch (validateError) {
-    if (validateError instanceof z.ZodError) {
-      const errorMessage = validateError.errors.map(
-        (e) => `${e.code}/ ${e.path}/ ${e.message}`
-      );
-
-      throw new ValidationError("[Validation Error] " + errorMessage);
-    }
-  }
+  cameraSchema.parse(cameraParams);
 
   return cameraParams;
 }
