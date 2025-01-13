fcf87fa8e82542eea7a4e8d0160187d9a073687c
songdabin <thdekqls1222@naver.com>
2025-01-13 15:16:19 +0900

    chore: delete makeExtrinsic
    - delete makeExtrinsic to avoid any type

--- lib/services/camera_model_parser.ts
+++ lib/services/camera_model_parser.ts
@@ -3,20 +3,6 @@ import { CameraModelType, CameraType } from "../types/type";
 export function cameraModelParser(
   data: Record<string, any>
 ): [CameraType, CameraModelType] {
-  const makeExtrinsic = (inputExtrinsic: any) => {
-    return {
-      frameFrom: inputExtrinsic.frame_from,
-      frameTo: inputExtrinsic.frame_to,
-      qw: inputExtrinsic.qw,
-      qx: inputExtrinsic.qx,
-      qy: inputExtrinsic.qy,
-      qz: inputExtrinsic.qz,
-      tx: inputExtrinsic.tx,
-      ty: inputExtrinsic.ty,
-      tz: inputExtrinsic.tz,
-    };
-  };
-
   return [
     data.type,
     {
@@ -27,9 +13,39 @@ export function cameraModelParser(
       height: data.height,
       width: data.width,
       intrinsic: data.intrinsic,
-      vcsExtrinsic: makeExtrinsic(data.vcs_extrinsic),
-      lcsExtrinsic: makeExtrinsic(data.lcs_extrinsic),
-      mvcsExtrinsic: makeExtrinsic(data.mvcs_extrinsic),
+      vcsExtrinsic: {
+        frameFrom: data.vcsExtrinsic.frame_from,
+        frameTo: data.vcsExtrinsic.frame_to,
+        qw: data.vcsExtrinsic.qw,
+        qx: data.vcsExtrinsic.qx,
+        qy: data.vcsExtrinsic.qy,
+        qz: data.vcsExtrinsic.qz,
+        tx: data.vcsExtrinsic.tx,
+        ty: data.vcsExtrinsic.ty,
+        tz: data.vcsExtrinsic.tz,
+      },
+      lcsExtrinsic: {
+        frameFrom: data.lcsExtrinsic.frame_from,
+        frameTo: data.lcsExtrinsic.frame_to,
+        qw: data.lcsExtrinsic.qw,
+        qx: data.lcsExtrinsic.qx,
+        qy: data.lcsExtrinsic.qy,
+        qz: data.lcsExtrinsic.qz,
+        tx: data.lcsExtrinsic.tx,
+        ty: data.lcsExtrinsic.ty,
+        tz: data.lcsExtrinsic.tz,
+      },
+      mvcsExtrinsic: {
+        frameFrom: data.mvcsExtrinsic.frame_from,
+        frameTo: data.mvcsExtrinsic.frame_to,
+        qw: data.mvcsExtrinsic.qw,
+        qx: data.mvcsExtrinsic.qx,
+        qy: data.mvcsExtrinsic.qy,
+        qz: data.mvcsExtrinsic.qz,
+        tx: data.mvcsExtrinsic.tx,
+        ty: data.mvcsExtrinsic.ty,
+        tz: data.mvcsExtrinsic.tz,
+      },
     },
   ];
 }
