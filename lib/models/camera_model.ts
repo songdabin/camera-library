15261915a32eb27dc63b821154cbd35ffaeef43a
songdabin <thdekqls1222@naver.com>
2025-01-15 16:43:10 +0900

    feat: vcsCuboidToIcsCuboidLines
    - vcsCuboidToIcsCuboidLines
    - vcsCuboidToCcsPoints
    - vcsCuboidToVcsPoiints

--- lib/models/camera_model.ts
+++ lib/models/camera_model.ts
@@ -1,5 +1,17 @@
-import { Extrinsic, ICSPoint, Intrinsic, Vector3Like } from "../types/type";
-import * as THREE from "three";
+import {
+  Cuboid,
+  Extrinsic,
+  ICSPoint,
+  Intrinsic,
+  VcsCuboidToCcsPointsArgs,
+  Vector3Like,
+} from "../types/type";
+import { Line3, Matrix4, Quaternion, Vector3 } from "three";
+import {
+  getHomogeneousTransformMatrix,
+  matrix4to3,
+  multiplyMatrix4,
+} from "./LtMatrix4";
 
 export abstract class CameraModel {
   channel: string;
@@ -42,21 +54,112 @@ export abstract class CameraModel {
     this.mvcsExtrinsic = mvcsExtrinsic;
   }
 
-  public projectVcsToCcs(vec3: THREE.Vector3): THREE.Vector3 {
+  public projectVcsToCcs(vec3: Vector3): Vector3 {
     const { qw, qx, qy, qz } = this.vcsExtrinsic;
-    const quaternion = new THREE.Quaternion(qx, qy, qz, qw);
+    const quaternion = new Quaternion(qx, qy, qz, qw);
 
-    const rotationMatrix = new THREE.Matrix4().makeRotationFromQuaternion(
-      quaternion
-    );
+    const rotationMatrix = new Matrix4().makeRotationFromQuaternion(quaternion);
 
     const rotatedVcsPoint = vec3.clone().applyMatrix4(rotationMatrix);
 
     const { tx, ty, tz } = this.vcsExtrinsic;
-    const translationVector = new THREE.Vector3(tx, ty, tz);
+    const translationVector = new Vector3(tx, ty, tz);
 
     return rotatedVcsPoint.add(translationVector);
   }
 
   abstract projectCcsToIcs(vec3: Vector3Like): ICSPoint;
+
+  abstract vcsCuboidToIcsCuboidLines(vcsCuboid: Cuboid, order: "zyx"): Line3[];
+
+  public getCcsLinesFromCuboid(cuboid: Cuboid, order: "zyx"): Line3[] {
+    const ccsPoints = this.vcsCuboidToCcsPoints({
+      vcsCuboid: cuboid,
+      order,
+    });
+
+    const flb: Vector3 = ccsPoints[0]; // front left bottom
+    const frb: Vector3 = ccsPoints[1]; // front right bottom
+    const frt: Vector3 = ccsPoints[2]; // front right top
+    const flt: Vector3 = ccsPoints[3]; // front left top
+    const rlb: Vector3 = ccsPoints[4]; // rear left bottom
+    const rrb: Vector3 = ccsPoints[5]; // rear right bottom
+    const rrt: Vector3 = ccsPoints[6]; // rear right top
+    const rlt: Vector3 = ccsPoints[7]; // rear left top
+
+    // Left -> Right, Front -> Rear, Bottom -> Top
+    const lines: Line3[] = [
+      new Line3(flt, frt),
+      new Line3(flt, rlt),
+      new Line3(rlt, rrt),
+      new Line3(frt, rrt),
+      new Line3(flb, frb),
+      new Line3(rlb, rrb),
+      new Line3(flb, rlb),
+      new Line3(frb, rrb),
+      new Line3(flb, flt),
+      new Line3(frb, frt),
+      new Line3(rlb, rlt),
+      new Line3(rrb, rrt),
+    ];
+
+    return lines;
+  }
+
+  private vcsCuboidToCcsPoints({
+    vcsCuboid,
+    order = "zyx",
+  }: VcsCuboidToCcsPointsArgs) {
+    const vcsPoints = this.vcsCuboidToVcsPoints(vcsCuboid, order);
+
+    const vcsPointArray = [];
+
+    for (let i = 0; i < 24; i += 3) {
+      vcsPointArray.push(
+        new Vector3(vcsPoints[i], vcsPoints[i + 1], vcsPoints[i + 2])
+      );
+    }
+
+    const ccsPointsArray: Vector3[] = [];
+
+    vcsPointArray.forEach((vcsPoint) => {
+      ccsPointsArray.push(this.projectVcsToCcs(vcsPoint));
+    });
+
+    return ccsPointsArray;
+  }
+
+  public vcsCuboidToVcsPoints(cuboid: Cuboid, order: "zyx") {
+    // prettier-ignore
+    const {
+        x: tx, y: ty, z: tz,
+        yaw, roll, pitch,
+        width, height, length,
+      } = cuboid;
+    const transformMatrix = getHomogeneousTransformMatrix({
+      angle: { yaw, roll, pitch },
+      translation: { tx, ty, tz },
+      order,
+    });
+
+    const [y, z, x] = [width / 2, height / 2, length / 2];
+    // prettier-ignore
+    const points = [
+      x, y, -z, 1, // front left bottom
+      x, -y, -z, 1, // front right bottom
+      x, -y, z, 1, // front right top
+      x, y, z, 1, // front left top
+      
+      -x, y, -z, 1, // rear left bottom
+      -x, -y, -z, 1, // rear right bottom
+      -x, -y, z, 1, // rear right top
+      -x, y, z, 1, // rear left top
+    ];
+    const vcsPoints = multiplyMatrix4(
+      points,
+      transformMatrix.transpose().elements()
+    );
+
+    return matrix4to3(vcsPoints);
+  }
 }
