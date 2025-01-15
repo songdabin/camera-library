f61f930b9b523966c4f0bd0edf596a9395f8a306
songdabin <thdekqls1222@naver.com>
2025-01-15 16:50:31 +0900

    feat: vcsCuboidToIcsCuboidLines
    - vcsCuboidToIcsCuboidLines

--- lib/models/rectilinear_model.ts
+++ lib/models/rectilinear_model.ts
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-import { ICSPoint } from "../types/type";
+import { Line3, Vector3 } from "three";
+import { Cuboid, ICSPoint } from "../types/type";
 import { CameraModel } from "./camera_model";
 
 export class RectilinearModel extends CameraModel {
@@ -16,7 +16,7 @@ export class RectilinearModel extends CameraModel {
     return [distortedX, distortedY];
   }
 
-  public projectCcsToIcs(vec3: THREE.Vector3): ICSPoint {
+  public projectCcsToIcs(vec3: Vector3): ICSPoint {
     const normalizedPoint = vec3.clone().divideScalar(vec3.getComponent(2));
 
     const { fx, fy, cx, cy } = this.intrinsic;
@@ -33,5 +33,50 @@ export class RectilinearModel extends CameraModel {
     };
 
     return distortedPoint;
+  }
+
+  public vcsCuboidToIcsCuboidLines(vcsCuboid: Cuboid, order: "zyx"): Line3[] {
+    const ccsLines = this.getCcsLinesFromCuboid(vcsCuboid, order);
+    const icsLines = this.ccsLinesToIcsLines(ccsLines);
+    return icsLines;
+  }
+
+  private ccsLinesToIcsLines(ccsLines: Line3[]) {
+    /*
+  private ccsLinesToIcsLines(ccsLines: [Vector3, Vector3][]) {
+    const v1 = new Vector3(...icsToVcsPoitns([0, this.height / 2, 150], this));
+    const v2 = new Vector3(
+      ...icsToVcsPoitns([this.width, this.height / 2, 150], this)
+    );
+    const dotProduct = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    const angle =
+      Math.acos(dotProduct / (v1.length() * v2.length())) * (180 / Math.PI);
+
+    const { lines, positiveMask } = getTruncatedLinesInCameraFov(
+      ccsLines,
+      this.channel,
+      angle
+    );
+    */
+    const icsLines: Line3[] = [
+      new Line3(new Vector3(1, 1, 1), new Vector3(1, 1, 1)),
+    ];
+
+    /*
+    const icsLines: Line3[] = lines.map((line: Line3, i: number) => {
+      if (positiveMask[i]) {
+        const icsP1 = this.projectCcsToIcs(line.start);
+        const icsP2 = this.projectCcsToIcs(line.end);
+        return [
+          { x: icsP1.x, y: icsP1.y },
+          { x: icsP2.x, y: icsP2.y },
+        ];
+      } else {
+        return null;
+      }
+    });
+    */
+
+    return icsLines;
   }
 }
