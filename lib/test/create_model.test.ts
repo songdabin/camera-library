a09d939a03d4c0c178aa54f1d199017630a86db2
songdabin <thdekqls1222@naver.com>
2025-01-17 16:45:15 +0900

    chore: separate test code
    - separate test code

--- lib/test/create_model.test.ts
+++ lib/test/create_model.test.ts
@@ -4,21 +4,14 @@ import { FisheyeModel } from "../models/fisheye_model";
 import { RectilinearModel } from "../models/rectilinear_model";
 import { createModel } from "../services/create_model";
 import { splitData } from "../services/split_data";
-import {
-  fisheyeProjectCcsToIcsTestCase,
-  projectVcsToCcsTestCase,
-  rectilinearProjectCcsToIcsTestCase,
-} from "./testcase";
 
 const frontFilePath = path.join(process.cwd(), "assets", "svc_front.yaml");
 const frontFileContent = fs.readFileSync(frontFilePath, "utf8");
 const [frontCameraType, frontCameraParams] = splitData(frontFileContent);
-const fisheyeModel = createModel(frontCameraType, frontCameraParams);
 
 const rearFilePath = path.join(process.cwd(), "assets", "svc_rear.yaml");
 const rearFileContent = fs.readFileSync(rearFilePath, "utf8");
 const [rearCameraType, rearCameraParams] = splitData(rearFileContent);
-const rectilinearModel = createModel(rearCameraType, rearCameraParams);
 
 test("Fisheye Model Create Test", () => {
   expect(createModel(frontCameraType, frontCameraParams)).toBeInstanceOf(
@@ -26,28 +19,10 @@ test("Fisheye Model Create Test", () => {
   );
 });
 
-fisheyeProjectCcsToIcsTestCase.forEach(({ input, output }) => {
-  test("Fisheye Model projectCcsToIcs Test", () => {
-    expect(fisheyeModel?.projectCcsToIcs(input)).toEqual(output);
-  });
-});
-
 test("Rectilinear Model Create Test", () => {
   expect(createModel(rearCameraType, rearCameraParams)).toBeInstanceOf(
     RectilinearModel
   );
-});
-
-projectVcsToCcsTestCase.forEach(({ input, output }) => {
-  test("Rectilinear Model projectVcsToCcs Test", () => {
-    expect(rectilinearModel?.projectVcsToCcs(input)).toEqual(output);
-  });
-});
-
-rectilinearProjectCcsToIcsTestCase.forEach(({ input, output }) => {
-  test("Rectilinear Model projectCcsToIcs Test", () => {
-    expect(rectilinearModel?.projectCcsToIcs(input)).toEqual(output);
-  });
 });
 
 // test("icsToVcsPoint Test", () => {
